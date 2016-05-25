3a614d9527405fea5f9903e3d44e36583ad1f7e8
Nicolette Chambers <nicolette3883@gmail.com>
2016-05-25 18:03:58 -0500

    Story #2 | Draw board when game starts

--- src/main/java/com/thoughtworks/continuinglearning/TicTacToe.java
+++ src/main/java/com/thoughtworks/continuinglearning/TicTacToe.java
@@ -1,20 +1,13 @@
 package com.thoughtworks.continuinglearning;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.PrintStream;
 
 public class TicTacToe {
-
     public static void main(String[] args) {
-
         PrintStream printStream = new PrintStream(System.out);
         Board board = new Board(printStream);
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        Player player = new Player(board, bufferedReader);
-
-
-        Game game = new Game(printStream, board, player, bufferedReader);
+        Player player = new Player();
+        Game game = new Game(board, player);
         game.start();
     }
 }
