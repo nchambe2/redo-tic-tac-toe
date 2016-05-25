4fb3d670308f7ab3fa11922648a6b30fa8d1f18d
Nicolette Chambers <nicolette3883@gmail.com>
2016-05-25 18:01:38 -0500

    Story #1 | Add draw board functionality

--- src/main/java/com/thoughtworks/continuinglearning/Board.java
+++ src/main/java/com/thoughtworks/continuinglearning/Board.java
@@ -2,42 +2,20 @@ package com.thoughtworks.continuinglearning;
 
 import java.io.PrintStream;
 
-/**
- * Created by nicollettec on 4/28/16.
- */
 public class Board {
-
-    public static final String X = "X";
-    public static final String O = "O";
-
     private PrintStream printStream;
-    private String board =  "1|2|3\n" +
-                            "-----\n" +
-                            "4|5|6\n" +
-                            "-----\n" +
-                            "7|8|9";
+    private String board = "1|2|3\n" +
+            "-----\n" +
+            "4|5|6\n" +
+            "-----\n" +
+            "7|8|9";
 
-    public Board(PrintStream printStream){
+    public Board(PrintStream printStream) {
 
         this.printStream = printStream;
     }
 
-    public void drawBoard(){
-        printStream.println(
-               board
-        );
-
-    }
-
-    public void mark(String playerMove) {
-        Integer userSelection = Integer.parseInt(playerMove);
-
-        if (userSelection % 2 == 0) {
-            board = board.replaceAll(playerMove, X);
-        } else {
-            board = board.replaceAll(playerMove, O);
-        }
-
-        drawBoard();
+    public void draw() {
+        printStream.println(board);
     }
 }
--- src/test/java/com/thoughtworks/continuinglearning/BoardTest.java
+++ src/test/java/com/thoughtworks/continuinglearning/BoardTest.java
@@ -3,8 +3,6 @@ package com.thoughtworks.continuinglearning;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.PrintStream;
 
 import static org.mockito.Mockito.mock;
@@ -12,21 +10,20 @@ import static org.mockito.Mockito.verify;
 
 
 public class BoardTest {
-
     private PrintStream printStream;
     private Board board;
-    private BufferedReader bufferedReader;
+
+
     @Before
-    public void setUp(){
+    public void setUp() {
         printStream = mock(PrintStream.class);
         board = new Board(printStream);
-        bufferedReader = mock(BufferedReader.class);
     }
 
     @Test
     public void shouldDrawBoard(){
 
-        board.drawBoard();
+        board.draw();
 
         verify(printStream).println(
                 "1|2|3\n" +
@@ -37,32 +34,5 @@ public class BoardTest {
         );
     }
 
-    @Test
-    public void shouldUpdateBoardWithXWhenUserMarksEvenSquare() throws IOException {
-
-        board.mark("2");
-
-        verify(printStream).println(
-                "1|X|3\n" +
-                "-----\n" +
-                "4|5|6\n" +
-                "-----\n" +
-                "7|8|9"
-        );
-    }
-
-    @Test
-    public void shouldUpdateBoardWithOWhenUserMarksOddSquare() {
-        board.mark("3");
-
-        verify(printStream).println(
-                "1|2|O\n" +
-                "-----\n" +
-                "4|5|6\n" +
-                "-----\n" +
-                "7|8|9"
-        );
-
-    }
 
 }
