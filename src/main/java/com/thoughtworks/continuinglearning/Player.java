a25f46bf750f8e8869f7acf8a8b1b76f693cbf8e
Nicolette Chambers <nicolette3883@gmail.com>
2016-06-02 14:41:58 -0500

    Refactor makeMove method to prompt the user for input && add condtional logic for making a move

--- src/main/java/com/thoughtworks/continuinglearning/Player.java
+++ src/main/java/com/thoughtworks/continuinglearning/Player.java
@@ -1,5 +1,7 @@
 package com.thoughtworks.continuinglearning;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintStream;
 
 public class Player {
@@ -7,22 +9,49 @@ public class Player {
     private String symbol;
     private String playerNumber;
     private PrintStream printstream;
+    private BufferedReader bufferedReader;
 
-    public Player(Board board, String symbol, String playerNumber, PrintStream printstream) {
+    public Player(Board board, String symbol, String playerNumber, PrintStream printstream, BufferedReader bufferedReader) {
         this.board = board;
         this.symbol = symbol;
         this.playerNumber = playerNumber;
         this.printstream = printstream;
+        this.bufferedReader = bufferedReader;
     }
 
     public void makeMove() {
-      board.mark(symbol);
-    }
+      printstream.println(playerMakeAMoveMessage());
+      Integer locationToMark = Integer.parseInt(getUserInput());
 
+      if(board.locationAvailable(locationToMark)) {
+          board.mark(locationToMark, symbol);
+      } else {
+          printstream.println(locationAlreadyTakenMessage());
+      }
+    }
 
     public void hasWon() {
         if(board.hasWinner()) {
             printstream.println("Player " + playerNumber + " Wins!");
         }
     }
+
+    private String playerMakeAMoveMessage() {
+        return "Player " + playerNumber + " please enter a number from 1 through 9.";
+    }
+
+    private String locationAlreadyTakenMessage() {
+         return "Location already taken";
+    }
+
+    private String getUserInput() {
+        try {
+            return bufferedReader.readLine();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
 }
--- src/test/java/com/thoughtworks/continuinglearning/PlayerTest.java
+++ src/test/java/com/thoughtworks/continuinglearning/PlayerTest.java
@@ -3,6 +3,8 @@ package com.thoughtworks.continuinglearning;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintStream;
 
 import static org.hamcrest.core.Is.is;
@@ -17,23 +19,19 @@ public class PlayerTest {
     private PrintStream printstream;
     private Board board;
     private String symbol;
+    private BufferedReader bufferedReader;
 
 
     @Before
     public void setUp() {
         symbol = "X";
+        bufferedReader = mock(BufferedReader.class);
         board = mock(Board.class);
         printstream = mock(PrintStream.class);
         playerNumber = "1";
-        player = new Player(board, symbol, playerNumber, printstream);
+        player = new Player(board, symbol, playerNumber, printstream, bufferedReader);
     }
 
-    @Test
-    public void shouldMarkBoardWhenPlayerMakesAMove() {
-        player.makeMove();
-
-        verify(board).mark(symbol);
-    }
 
     @Test
     public void shouldCheckToSeeIfTheirIsAWinAfterPlayerHasMadeAMove() {
@@ -49,6 +47,35 @@ public class PlayerTest {
 
         verify(printstream).println("Player " + playerNumber + " Wins!");
     }
+
+    @Test
+    public void shouldPrintMessageToUserWhenPlayerMakesAMove() throws IOException {
+        when(bufferedReader.readLine()).thenReturn("1");
+        player.makeMove();
+        verify(printstream).println("Player " + playerNumber + " please enter a number from 1 through 9.");
+    }
+
+    @Test
+    public void shouldMarkLocationOneWhenLocationIsEmpty() throws IOException {
+        when(bufferedReader.readLine()).thenReturn("1");
+        when(board.locationAvailable(1)).thenReturn(true);
+
+        player.makeMove();
+
+        verify(board).mark(1, symbol);
+    }
+
+    @Test
+    public void shouldReturnLocationUnavailableMessageWhenLocationIsFull() throws IOException {
+        when(bufferedReader.readLine()).thenReturn("1");
+        when(board.locationAvailable(1)).thenReturn(false);
+
+        player.makeMove();
+
+        verify(printstream).println("Location already taken");
+
+    }
+
 
 
 }
